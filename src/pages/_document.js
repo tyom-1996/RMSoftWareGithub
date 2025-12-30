005aeb7c851839c28d67ce5a135b4694284b4f1d
a.akobjanyan <a.akobjanyan@sebekon.ru>
2025-12-31 02:24:38 +0400

    fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,9 +12,12 @@ class MyDocument extends Document {
             content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, viewport-fit=cover"
           />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
 
         <body>
